b17fb213a98bba5189e4ae5ba009632c9bcd992e
huong9x <huongnv@sphinx-software.com>
2019-07-08 20:43:05 +0700

    nothing here

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align='center'>This is NOTHING</h1>
+<p align='center'><i>for educational purposes only, i don't own anything!</i></p>
--- src/MyHeavyService.js
+++
@@ -1,8 +0,0 @@
-import {singleton} from '@fusion.io/framework';
-
-@singleton()
-export default class MyHeavyService {
-    provideData() {
-        return new Promise(resolve => setTimeout(() => {resolve(new Date())}, 3000));
-    }
-}
